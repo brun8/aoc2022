8aef365013551d6ce5d20e6b5f2ec417fda54547
bruno <brunofvteixeira@gmail.com>
2022-12-04 11:57:23 -0300

    4

---
+++ 0/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+  lines := readLines()
+
+  fmt.Println(lines)
+}
+
+func readLines() []string {
+  f, _ := os.Open("input.txt")
+  defer f.Close()
+
+  lines := make([]string, 0)
+  scanner := bufio.NewScanner(f)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines
+}
+

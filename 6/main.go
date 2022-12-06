f664d2e3907104c925c44651eda473ea7d61298b
bruno <brunofvteixeira@gmail.com>
2022-12-06 16:21:33 -0300

    go fmt

--- 6/main.go
+++ 6/main.go
@@ -11,24 +11,24 @@ func main() {
 
 	for _, line := range lines {
 		for i := 0; i < len(line)-13; i++ {
-      m := make(map[byte]bool)
-      t := 0
+			m := make(map[byte]bool)
+			t := 0
 			for j := i; j < i+14; j++ {
-        if !m[line[j]] {
-          t++
-          m[line[j]] = true
-        }
+				if !m[line[j]] {
+					t++
+					m[line[j]] = true
+				}
 			}
-      if t == 14 {
-        fmt.Println(i+14)
-        break
-      }
+			if t == 14 {
+				fmt.Println(i + 14)
+				break
+			}
 		}
 	}
 }
 
 func readLines() []string {
-  f, _ := os.Open("input.txt")
+	f, _ := os.Open("input.txt")
 	//f, _ := os.Open("input_test.txt")
 	defer f.Close()
 
